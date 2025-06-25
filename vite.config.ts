bb05eb1d66eba62e0a0e8fb66a269020a2c08740
sojebsikder <sojebsikder@gmail.com>
2025-06-25 10:14:58 +0600

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/lib/editor/index.ts'),
-      name: 'Sitor',
-      fileName: (format) => `sitor.${format}.js`,
+      entry: path.resolve(__dirname, "src/lib/editor/index.ts"),
+      name: "Soditor",
+      fileName: (format) => `soditor.${format}.js`,
     },
     rollupOptions: {
       external: [], // list dependencies here to avoid bundling them
